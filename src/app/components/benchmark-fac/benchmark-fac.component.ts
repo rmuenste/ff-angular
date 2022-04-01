521143a71a0042bf3fa80a2c35efbc79b45a6634
Raphael Muenster <raphael.muenster@gmail.com>
2022-04-01 23:30:41 +0200

    Worked on description of fac benchmark

--- src/app/components/benchmark-fac/benchmark-fac.component.ts
+++ src/app/components/benchmark-fac/benchmark-fac.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 export interface PeriodicElement {
-  p1: number;
+  name: string;
   position: number;
-  p2: number;
-  mu1: number;
-  mu2: number;
-  g: number;
-  sigma: number;
-  re: number;
-  eo: number;
-  rel: number;
-  relmu: number;
+  weight: number;
+  symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, p1: 1000, p2: 100, mu1: 10, mu2: 1, g: 0.98, sigma: 24.5, re: 35, eo: 10, rel: 10, relmu: 10},
-  {position: 2, p1: 1000, p2: 1, mu1: 10, mu2: 0.1, g: 0.98, sigma: 1.96, re: 35, eo: 125, rel: 1000, relmu: 100},
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
 ];
+
 
 @Component({
   selector: 'app-benchmark-fac',
@@ -26,7 +28,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class BenchmarkFacComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'p1', 'p2', 'mu1', 'mu2', 'g', 'sigma', 're', 'eo', 'rel', 'relmu'];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
 
   mathEq = `When $ a \\ne 0 $`;
