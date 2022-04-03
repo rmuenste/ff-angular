ac7e7b51e59ba0c8cb70a6a7f53c13a527c24cab
Raphael Muenster <raphael.muenster@gmail.com>
2022-04-03 14:33:36 +0200

    Added the reference data section to 3D FAC

--- src/app/components/benchmark-fac/benchmark-fac.component.ts
+++ src/app/components/benchmark-fac/benchmark-fac.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface DragTime {
+  case: string;
+  cells: number;
+  dragmax: number;
+  liftmax: number;
+  liftmin: number;
+  tstep: number;
+  time: string;
+}
+
 export interface PeriodicElement {
   name: string;
   level: string;
@@ -7,6 +17,16 @@ export interface PeriodicElement {
   dofp: number;
   doft: number;
   cells: string;
+}
+
+export interface ReferenceElement {
+  column: number;
+  quantity: string;
+}
+
+export interface FileDownload {
+  file: string;
+  action: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
@@ -24,6 +44,23 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {level: "", name: 'FeatFlow', cells: "", dofu: 77177104, dofp: 12582912, doft: 89760016},
 ];
 
+const dragTimeData: DragTime[] = [
+{case: "FFL1", cells: 6144, dragmax: 3.2207, liftmax: 0.0027, liftmin: -0.0095, tstep: 0.010, time: "3220 x 2"},
+{case: "FFL2", cells: 49152, dragmax: 3.2877, liftmax: 0.0028, liftmin: -0.010892, tstep: 0.010, time: "17300 x 4"},
+{case: "FFL3", cells: 393216, dragmax: 3.2963, liftmax: 0.0028, liftmin: -0.010992, tstep: 0.010, time: "35550 x 24"},
+{case: "FFL4", cells: 3145728, dragmax: 3.2978, liftmax: 0.0028, liftmin: -0.010999, tstep: 0.005, time: "214473 x 48"},
+];
+
+const referenceData: ReferenceElement[] = [
+  {column: 1, quantity: "Time"},
+  {column: 2, quantity: "Drag"},
+  {column: 3, quantity: "Lift"},
+  {column: 4, quantity: "Z-Force"},
+];
+
+const fileData: FileDownload[] = [
+  {file: "BenchValues.txt", action: "Download"},
+];
 
 @Component({
   selector: 'app-benchmark-fac',
@@ -34,6 +71,15 @@ export class BenchmarkFacComponent implements OnInit {
 
   displayedColumns: string[] = ['level', 'cells', 'name', 'dofu', 'dofp', 'doft'];
   dataSource = ELEMENT_DATA;
+
+  displayedColumnsDragTime: string[] = ['case', 'cells', 'dragmax', 'liftmax', 'liftmin', 'tstep', 'time'];
+  dataSourceDragTime = dragTimeData;
+
+  displayedColumnsRefData: string[] = ['column', 'quantity'];
+  dataSourceRefData = referenceData;
+
+  displayedColumnsFiles: string[] = ['file', 'action'];
+  dataSourceFiles = fileData;
 
   mathEq = `When $ a \\ne 0 $`;
 
